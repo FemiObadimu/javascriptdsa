7be7fe83dda3a962df5eae636da123715db98298
femiobadimu <ficazzosam@gmail.com>
2023-07-22 14:34:08 +0100

    The Big O of n to the power of 2

--- O(nsquare).js
+++ O(nsquare).js
@@ -9,6 +9,3 @@ function logItems(n) {
 }
 
 logItems(10);
-
-// This above code runs at O(ncude) which is the same this as O(nsquare) in time complexity
-// In Conclusion a code with O(n) is much more better than a code of O(nsquare or ncude)
